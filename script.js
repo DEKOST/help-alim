0003e334c4896d6fd616fa7dd4d742f835ba7a2e
DEKOST <121599819+DEKOST@users.noreply.github.com>
2024-12-09 11:54:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,24 +10,31 @@ document.addEventListener('DOMContentLoaded', function() {
 
     clickButton.addEventListener('click', function() {
         score++;
-        scoreDisplay.textContent = `Score: ${score}`;
+        scoreDisplay.textContent = `$${score}`;
+        checkScore();
+    });
+
+    gameImage.addEventListener('click', function() {
+        score ++;
+        scoreDisplay.textContent = `$${score}`;
         checkScore();
     });
 
     secretButton.addEventListener('click', function() {
-        if (score < 5) {
+        if (score < 100) {
             hint.style.display = 'block';
         }
     });
 
     function checkScore() {
-        if (score >= 5 && !add20000ButtonClicked) {
-            secretButton.textContent = 'Add 20000 Points';
+        if (score >= 100 && !add20000ButtonClicked) {
+            secretButton.textContent = '!!! RUG PULL !!!';
+            secretButton.style.backgroundColor = 'red';
             secretButton.removeEventListener('click', secretButton.click);
             secretButton.addEventListener('click', function() {
                 if (!add20000ButtonClicked) {
                     score += 20000;
-                    scoreDisplay.textContent = `Score: ${score}`;
+                    scoreDisplay.textContent = `$${score}`;
                     //secretButton.disabled = true;
                     add20000ButtonClicked = true;
                     gameImage.src = '2.png'; // Изменить изображение на 2.png
