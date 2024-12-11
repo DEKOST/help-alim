10ef94cdc198cbc23913b796113a801f56e895d1
DEKOST <121599819+DEKOST@users.noreply.github.com>
2024-12-11 13:20:53 +0300

    Update saveUserData.js

--- netlify/functions/saveUserData.js
+++ netlify/functions/saveUserData.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const databasePath = path.join(__dirname, 'database.json');
+const databasePath = path.join(__dirname, './database.json');
 
 // Функция для чтения данных из JSON-файла
 function readDatabase() {
