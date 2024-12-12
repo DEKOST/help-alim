556edfae6d56240d20f60f2df47b4e987e20c13b
DKostenkov <kpstenkov007@gmail.com>
2024-12-12 10:33:38 +0300

    rename collection saveUserData

--- netlify/functions/saveUserData.js
+++ netlify/functions/saveUserData.js
@@ -1,10 +1,9 @@
 const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGODB_URI;
-console.log('Connecting to MongoDB with URI:', uri);
 const dbName = process.env.MONGODB_DBNAME;
-console.log('Connecting to MongoDB with URI:', dbName);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoClient = new MongoClient(uri);
+const clientPromise = mongoClient.connect();
 
 exports.handler = async function(event, context) {
     try {
@@ -18,7 +17,7 @@ exports.handler = async function(event, context) {
             throw new Error("Missing userId or username");
         }
 
-        await client.connect();
+        const client = await clientPromise;
         const database = client.db(dbName);
         const collection = database.collection('app');
 
@@ -38,7 +37,5 @@ exports.handler = async function(event, context) {
             statusCode: 500,
             body: JSON.stringify({ error: error.message })
         };
-    } finally {
-        await client.close();
     }
 };
